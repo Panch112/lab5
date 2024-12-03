d82d3535eebc015775251e85230ae0a633cedf4a
kwesaik <pbazenova96@gmail.com>
2024-12-04 01:51:05 +0300

    Create dialogfragment.java

---
+++ dialogfragment.java
@@ -0,0 +1,5 @@
+public class DialogFragment extends Fragment{
+ public DialogFragment(int LayoutType, int FragmentWidth, int FragmentHeigth, int ColumnsCount, int LinesCount, String TextColor, String BackgroundColor){
+ super(LayoutType, FragmentWidth, FragmentHeigth, ColumnsCount, LinesCount, TextColor, BackgroundColor);
+ }
+}
